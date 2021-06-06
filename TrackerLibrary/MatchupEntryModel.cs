3ba60ed75e34ab4423c4e81ba6ff1f375db5c715
Devilko0l <nguyen.tcao96@gmail.com>
2021-06-06 13:19:37 +0200

    Add tournamentViewerForm class

--- TrackerLibrary/MatchupEntryModel.cs
+++ TrackerLibrary/MatchupEntryModel.cs
@@ -8,10 +8,25 @@ namespace TrackerLibrary
 {
     public class MatchupEntryModel
     {
+        /// <summary>
+        /// Represent one team in the matchup.
+        /// </summary>
         public TeamModel TeamCompeting { get; set; }
 
+        /// <summary>
+        /// Represents the score for this particular team.
+        /// </summary>
         public double Score { get; set; }
 
+        /// <summary>
+        /// Represents the matchup that this team cam
+        /// from as the winner.
+        /// </summary>
         public MatchupModel ParentMatchup { get; set; }
+        
+        public MatchupEntryModel(double initialScore)
+        {
+            Console.WriteLine();
+        }
     }
 }
